d9b92d2224481cde93007e4cc1d5cfe0f7af4cbf
indahtambunan <55008554+indahtambunan@users.noreply.github.com>
2021-01-15 15:49:57 +0700

    15 Januari 2021

---
+++ resources/views/template/master.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>EVOTE.id - @yield('title')</title>
+
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+
+
+</head>
+
+<body>
+
+  @include('template.navbar')
+
+  <!-- ISI -->
+
+  @yield('content')
+
+  @include('template.footer')
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,25 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home/profil_saya/{id}', 'HomeController@profil_saya')->name('home.profil_saya');
+Route::get('/home/edit/{id}', 'HomeController@edit')->name('home.edit');
+Route::post('/home/update/{id}', 'HomeController@update')->name('home.update');
+
+Route::get('/user', 'UserController@index');
+Route::get('/user/create', 'UserController@create')->name('user.create');
+Route::post('/user', 'UserController@store')->name('user.store');
+Route::get('/user/{id}', 'UserController@detail')->name('user.detail');
+Route::post('/user/delete/{id}', 'UserController@destroy')->name('user.destroy');
+Route::get('/user/edit/{id}', 'UserController@edit')->name('user.edit');
+Route::post('/user/update/{id}', 'UserController@update')->name('user.update');
+Route::post('/users/simpan_status/{id}', 'UserController@simpan_status')->name('user.simpan_status');
+
