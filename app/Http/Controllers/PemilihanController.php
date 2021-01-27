37f251b64771a86a8bd60c19e672a14a0c5008d7
Siroja Nuril Hidayah <54984549+sirojanuril@users.noreply.github.com>
2021-01-27 16:14:20 +0700

    revisi

--- app/Http/Controllers/PemilihanController.php
+++ app/Http/Controllers/PemilihanController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use \App\Masyarakat;
 use \App\Pemilihan;
 use \App\Hasil;
+use \App\Periode;
 use Auth;
 use DB;
 
@@ -18,42 +19,62 @@ class PemilihanController extends Controller
 
     public function index()
     {
-    	$data = DB::table('masyarakat')
+        $tanggal_awal   = Periode::orderBy('tanggal', 'asc')->first();
+        $tanggal_akhir  = Periode::orderBy('tanggal', 'desc')->first();
+
+        $data = DB::table('masyarakat')
                     ->join('pemilihan', 'pemilihan.masyarakat_id', '=', 'masyarakat.id')
                     ->get();
 
-    	return view('pemilihan.index', compact('data'));
+        return view('pemilihan.index', compact('data', 'tanggal_awal', 'tanggal_akhir'));
     }
 
     public function pilih_kandidat(Request $request, $id)
     {
-    	$masyarakat    = Masyarakat::where('id', $id)->first();
-    	$pemilihan     = Pemilihan::where('id', $id)->first();
+        $masyarakat    = Masyarakat::where('id', $id)->first();
+        $pemilihan     = Pemilihan::where('id', $id)->first();
 
-    	$hasil = new Hasil();
-        $hasil->masyarakat_id 	= Auth::user()->id;
-        $hasil->pemilihan_id	= $pemilihan->id;
-        // $hasil->hasil_pilihan	= $request->nomor;
-        $hasil->save();
+        $tanggal_sekarang = date('Y-m-d');
+        $cek_tanggal = Periode::where('tanggal', $tanggal_sekarang)->count();
 
-    	return view('pemilihan.grafik', compact('hasil'));
+        if ($cek_tanggal > 0) {
+
+            $hasil = new Hasil();
+            $hasil->masyarakat_id   = Auth::user()->id;
+            $hasil->pemilihan_id    = $pemilihan->id;
+            $hasil->save();
+
+            // $hasil = Hasil::firstOrCreate(
+            //     ['masyarakat_id'=>\Auth::user()->id],
+            //     ['pemilihan_id'=>$id,'masyarakat_id'=>\Auth::user()->id]
+            // );
+
+            return redirect('pemilihan');
+
+        }else{
+            return redirect()->back();
+        }
+        
+
+        // return view('pemilihan.grafik', compact('hasil'));
+        
     }
 
     public function grafik()
     {
-    	$hasil = [];
+        $hasil = [];
 
-    	$pemilihan = Pemilihan::get();
+        $pemilihan = Pemilihan::get();
         
-    	foreach ($pemilihan as $key => $pilihan) {
-    		$pemilihan_id = $pilihan->id;
-    		$no_urut      = $pilihan->nomor_urut;
-    		$total        = Hasil::where('pemilihan_id', $pemilihan_id)->count();
+        foreach ($pemilihan as $key => $pilihan) {
+            $pemilihan_id = $pilihan->id;
+            $no_urut      = $pilihan->nomor_urut;
+            $total        = Hasil::where('pemilihan_id', $pemilihan_id)->count();
 
-    		$a['name']    = 'Nomor Urut: '.$no_urut;
-    		$a['y']       = $total;
-    		array_push($hasil, $a);
-    	}
-    	return view('pemilihan.grafik', compact('hasil'));
+            $a['name']    = 'Nomor Urut: '.$no_urut;
+            $a['y']       = $total;
+            array_push($hasil, $a);
+        }
+        return view('pemilihan.grafik', compact('hasil'));
     }
 }
